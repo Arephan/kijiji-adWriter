1060c69cc0a9b0a15c1b14a9f1d96ef43635d26d
han kim <hankim@hans-MacBook-Air.local>
2019-09-02 11:41:06 -0400

    add more iphonePics colors

--- constants.js
+++ constants.js
@@ -2,15 +2,26 @@ const forSale = [{
     name: "iphone",
     phoneNumber: "438 793 1474‬",
 
-    types: [{ name: "iphone 6 16gb", price: 185, dirName: "iphone-6-16gb" },
-    { name: "iphone 6 32gb", price: 195, dirName: "iphone-6-32gb" },
-    { name: "iphone 6 64gb", price: 215, dirName: "iphone-6-64gb" },
-    { name: "iphone 6s 16gb", price: 215, dirName: "iphone-6s-16gb" },
-    { name: "iphone 6s 32gb", price: 235, dirName: "iphone-6s-32gb" },
-    { name: "iphone 6s 64gb", price: 255, dirName: "iphone-6s-64gb" }],
-    colors: ["space grey", "rose gold", "silver", "gold"],
+    types: [
+        // { name: "iphone 6 16gb", price: 185, dirName: "iphone-6-16gb" },
+        // { name: "iphone 6 32gb", price: 195, dirName: "iphone-6-32gb" },
+        // { name: "iphone 6 64gb", price: 215, dirName: "iphone-6-64gb" },
+        { name: "iphone 6s 16gb", price: 215, dirName: "iphone-6s-16gb" },
+        { name: "iphone 6s 32gb", price: 235, dirName: "iphone-6s-32gb" }
+        // { name: "iphone 6s 64gb", price: 255, dirName: "iphone-6s-64gb" },
+    ],
+    colors: ["grey", "rose", "silver", "gold"],
 
 }]
+
+const personalKijijiAccounts = [
+    {email : "hanilk2006@gmail.com", password: "200639"},
+    {email : "hankimprod@gmail.com", password: "Helpyourself123*"}
+]
+
+const companyKijijiAcounts = [
+    {email: "6@hankimproductions.com", password: "Kijijiforlife1*"}
+]
 
 module.exports = {
     forSale: forSale
