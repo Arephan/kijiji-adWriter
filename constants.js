e5aa383d1e005349bd2455f0553b540cf103d1fe
han kim <hankim@hans-MacBook-Air.local>
2019-10-29 16:44:04 -0400

    Take my personal accounts out

--- constants.js
+++ constants.js
@@ -6,25 +6,22 @@ const forSale = [{
         // { name: "iphone 6 16gb", price: 185, dirName: "iphone-6-16gb" },
         // { name: "iphone 6 32gb", price: 195, dirName: "iphone-6-32gb" },
         // { name: "iphone 6 64gb", price: 215, dirName: "iphone-6-64gb" },
-        { name: "iphone 6s 16gb", price: 215, dirName: "iphone-6s-16gb" },
-        { name: "iphone 6s 32gb", price: 235, dirName: "iphone-6s-32gb" }
+        { name: "iphone 6s 16gb", price: 180, dirName: "iphone-6s-16gb" },
+        { name: "iphone 6s 32gb", price: 200, dirName: "iphone-6s-32gb" }
         // { name: "iphone 6s 64gb", price: 255, dirName: "iphone-6s-64gb" },
     ],
     colors: ["grey", "rose", "silver", "gold"],
+    condition: ["mint", "excellent", "10/10", "amazing", "awesome", "great"]
 
 }]
 
 const personalKijijiAccounts = [
-    {email : "hanilk2006@gmail.com", password: "200639"},
-    {email : "hankimprod@gmail.com", password: "Helpyourself123*"}
+    {email : "example@gmail.com", password: "examplepass"},
+    {email : "example2@gmail.com", password: "example2pass"}
 ]
 
-const companyKijijiAcounts = [
-    {email: "6@hankimproductions.com", password: "Kijijiforlife1*"}
-]
 
 module.exports = {
     forSale: forSale,
-    personalKijijiAccounts,
-    companyKijijiAcounts
+    personalKijijiAccounts
 }
